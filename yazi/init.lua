388f126392809b82d6b02cc9b9b9bbc5d1a5c925
Raimguzhinov <d.raimguzhinov@stud.sibguti.ru>
2024-05-16 08:55:49 +0700

    Edit same hypland conf

--- yazi/init.lua
+++
@@ -1,7 +0,0 @@
- function Header:render(area)
- 	self.area = area
-  		:constraints({ ui.Constraint.Percentage(50), ui.Constraint.Percentage(50) })
- 		:split(area)
-  	local right = ui.Line { self:tabs() }
- 	return {
- 		ui.Paragraph(chunks[1], { left }),
