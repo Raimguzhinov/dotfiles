03fad526eda8e517bc46afa85216b9bd5d544e7f
Dias B. Raimguzhinov <raimguzhinov@protei-lab.ru>
2025-11-21 02:44:06 +0700

    nixos: add yubikey-agent

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -256,6 +256,7 @@
   # Yubikey
   services.udev.packages = [ pkgs.yubikey-personalization ];
   services.pcscd.enable = true;
+  services.yubikey-agent.enable = true;
 
   security.polkit.enable = true; # polkit
   security.pam.services.swaylock = { };
